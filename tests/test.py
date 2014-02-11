5c36d8cc45d2644187f7375bac48f22bbf56820f
George Macon <george.macon@gmail.com>
2014-02-11 14:18:50 -0500

    Add unit tests for Spreadsheet properties

--- tests/test.py
+++ tests/test.py
@@ -71,6 +71,14 @@ class SpreadsheetTest(GspreadTest):
         title = self.config.get('Spreadsheet', 'title')
         self.spreadsheet = self.gc.open(title)
 
+    def test_properties(self):
+        self.assertEqual(self.config.get('Spreadsheet', 'id'),
+                         self.spreadsheet.id)
+        self.assertEqual(self.config.get('Spreadsheet', 'key'),
+                         self.spreadsheet.key)
+        self.assertEqual(self.config.get('Spreadsheet', 'title'),
+                         self.spreadsheet.title)
+
     def test_sheet1(self):
         sheet1 = self.spreadsheet.sheet1
         self.assertTrue(isinstance(sheet1, gspread.Worksheet))
